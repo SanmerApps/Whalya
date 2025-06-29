ca9c655f5a6db87a182352055bc2c6bd18cbfaeb
SanmerDev <sanmer@sanmer.dev>
2025-06-29 17:31:37 +0800

    Add Search for LicensesScreen

--- app/src/main/kotlin/dev/sanmer/whalya/ui/screens/licenses/LicensesViewModel.kt
+++ app/src/main/kotlin/dev/sanmer/whalya/ui/screens/licenses/LicensesViewModel.kt
@@ -8,9 +8,12 @@ import androidx.lifecycle.viewModelScope
 import dev.sanmer.whalya.Logger
 import dev.sanmer.whalya.model.LoadData
 import dev.sanmer.whalya.model.LoadData.Default.asLoadData
+import dev.sanmer.whalya.model.LoadData.Default.getValue
 import dev.sanmer.whalya.model.ui.UiLicense
 import dev.sanmer.whalya.repository.LicensesRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class LicensesViewModel(
@@ -19,11 +22,32 @@ class LicensesViewModel(
     var data by mutableStateOf<LoadData<List<UiLicense>>>(LoadData.Loading)
         private set
 
+    var isSearch by mutableStateOf(false)
+        private set
+
+    private var cache = emptyList<UiLicense>()
+    private val keyFlow = MutableStateFlow("")
+
     private val logger = Logger.Android("LicensesViewModel")
 
     init {
         logger.d("init")
         loadData()
+        keyObserver()
+    }
+
+    fun toggleSearch() {
+        isSearch = !isSearch
+        cache = if (isSearch) {
+            data.getValue(emptyList()) { it }
+        } else {
+            search("")
+            emptyList()
+        }
+    }
+
+    fun search(value: String) {
+        keyFlow.value = value
     }
 
     private fun loadData() {
@@ -33,6 +57,24 @@ class LicensesViewModel(
             }.onFailure {
                 logger.e(it)
             }.asLoadData()
+        }
+    }
+
+    private fun keyObserver() {
+        viewModelScope.launch {
+            keyFlow.collectLatest { key ->
+                data = LoadData.Success(
+                    cache.filter {
+                        it.original.name.contains(
+                            key, ignoreCase = true
+                        ) || it.original.groupId.contains(
+                            key, ignoreCase = true
+                        ) || it.original.artifactId.contains(
+                            key, ignoreCase = true
+                        )
+                    }
+                )
+            }
         }
     }
 }
