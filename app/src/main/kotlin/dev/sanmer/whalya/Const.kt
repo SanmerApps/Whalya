c1198c83cb30b48695aff11e45b3f7ac653bb19c
SanmerDev <sanmer@sanmer.dev>
2025-10-08 17:01:23 +0800

    Override artifacts.json report path

--- app/src/main/kotlin/dev/sanmer/whalya/Const.kt
+++ app/src/main/kotlin/dev/sanmer/whalya/Const.kt
@@ -7,5 +7,5 @@ object Const {
     const val CERT_DEMO = "-----BEGIN CERTIFICATE-----\n-----END CERTIFICATE-----"
     const val KEY_DEMO = "-----BEGIN (EC/RSA) PRIVATE KEY-----\n-----END (EC/RSA) PRIVATE KEY-----"
 
-    const val LICENSEE_PATH = "app/cash/licensee/artifacts.json"
+    const val LICENSEE_PATH = "artifacts.json"
 }
